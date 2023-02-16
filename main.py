41c0579c53c88e2dde9bddfb7adb1f4572dddf1c
ZephyrUW <mccracken.scott@gmail.com>
2023-02-16 15:35:06 -0700

    Initial - Create main file & upload results files

---
+++ main.py
@@ -0,0 +1,26 @@
+# Virtual Environment
+# https://linuxopsys.com/topics/create-python-virtual-environment-on-ubuntu
+
+# Scrape prior tournaments (based on date??) to get 1-3 place finishers.
+# Track throughout season.  Create race?? graph.
+# Email results
+
+# Bump Plot https://towardsdatascience.com/7-visualizations-with-python-to-express-changes-in-rank-over-time-71c1f11d7e4b
+# https://github.com/kartikay-bagla/bump-plot-python/blob/master/bumpplot.py
+
+import openpyxl
+import pandas
+
+from rich import print, traceback
+traceback.install()
+
+# Get list of pdf or xlsm
+# Loop through and get 1-3 place
+# ?? Save to db
+# Create Bump Plot
+# Email Results
+# Export HTML?
+
+if __name__ == "__main__":
+    print("Start")
+    
